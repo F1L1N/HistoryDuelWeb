9b6c6c1541528f3486b63cc782d396e8fafd8b1b
F1L1N <pavel.filin.rm@gmail.com>
2020-04-30 12:53:52 +0300

    JWT install #1

--- config/app.php
+++ config/app.php
@@ -175,6 +175,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Custom Service Providers...
+         */
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+
     ],
 
     /*
