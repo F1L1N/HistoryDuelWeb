eb2184f46d36ec9fc30acfc8fcd38793a7cff5ac
F1L1N <pavel.filin.rm@gmail.com>
2020-05-03 17:53:37 +0300

    Add landing t\emplate

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,23 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.scripts([
+        'resources/assets/js/jquery-1.11.1.min.js',
+        'resources/assets/js/bootstrap.min.js',
+        'resources/assets/js/slick.min.js',
+        'resources/assets/js/placeholdem.min.js',
+        'resources/assets/js/rs-plugin/js/jquery.themepunch.plugins.min.js',
+        'resources/assets/js/rs-plugin/js/jquery.themepunch.revolution.min.js',
+        //'resources/assets/js/waypoints.min.js',
+        //'resources/assets/js/jquery.waypoints.js',
+        'resources/assets/js/scripts.js',
+        'resources/assets/js/modernizr.custom.32033.js',
+    ], 'public/js/app.js')
+    .styles([
+        'resources/assets/css/vendor/bootstrap.min.css',
+        'resources/assets/css/animate.css',
+        'resources/assets/css/font-awesome.min.css',
+        'resources/assets/css/slick.css',
+        'resources/assets/js/rs-plugin/css/settings.css',
+        'resources/assets/css/styles.css',
+    ], 'public/css/app.css');
